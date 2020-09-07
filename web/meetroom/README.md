208857eed6342412fcd2ab4d6df7c59d5e3b5c4f
mochalyur1994 <37665892+mochalyur1994@users.noreply.github.com>
2020-09-07 13:41:38 +0700

    All In

---
+++ web/meetroom/README.md
@@ -0,0 +1,20 @@
+# chatRoom
+
+This is an online chat room system where users can log in and have chats. This provides 2 types of chatting.
+
+1. Public: Anyone can join the group and have conversation
+2. Private: You require a password to join the group.
+
+The admin access control has the rights to manage the groups and the users.
+
+Preview of this website is available over: 
+```
+https://chatbot-rayan.000webhostapp.com/
+```
+
+Credentials for the system
+```
+Username: test
+Password: test
+Password for chat with name "chatroom password": leeann
+```
---
+++ web/meetroom/chat_system.sql
@@ -0,0 +1,148 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 07, 2020 at 08:36 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `chat_system`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chat`
+--
+
+CREATE TABLE `chat` (
+  `chatid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `chatroomid` int(11) NOT NULL,
+  `message` varchar(200) NOT NULL,
+  `chat_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chatroom`
+--
+
+CREATE TABLE `chatroom` (
+  `chatroomid` int(11) NOT NULL,
+  `chat_name` varchar(60) NOT NULL,
+  `date_created` datetime NOT NULL,
+  `chat_password` varchar(30) NOT NULL,
+  `userid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chat_member`
+--
+
+CREATE TABLE `chat_member` (
+  `chat_memberid` int(11) NOT NULL,
+  `chatroomid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `userid` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(150) NOT NULL,
+  `uname` varchar(60) NOT NULL,
+  `photo` varchar(200) NOT NULL,
+  `access` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`userid`, `username`, `password`, `uname`, `photo`, `access`) VALUES
+(4, 'rayandas', '97a675dbf726a817d47060f0aa81f7a9', 'Rayan', '', 2),
+(5, 'vijay', '202cb962ac59075b964b07152d234b70', 'Vijay', '', 2),
+(7, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Admin', '', 1),
+(8, 'mochalyur', 'dac734b91e91a8caf6f4c10daf6f34ee', 'Moch Alyur', '', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `chat`
+--
+ALTER TABLE `chat`
+  ADD PRIMARY KEY (`chatid`);
+
+--
+-- Indexes for table `chatroom`
+--
+ALTER TABLE `chatroom`
+  ADD PRIMARY KEY (`chatroomid`);
+
+--
+-- Indexes for table `chat_member`
+--
+ALTER TABLE `chat_member`
+  ADD PRIMARY KEY (`chat_memberid`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `chat`
+--
+ALTER TABLE `chat`
+  MODIFY `chatid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `chatroom`
+--
+ALTER TABLE `chatroom`
+  MODIFY `chatroomid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `chat_member`
+--
+ALTER TABLE `chat_member`
+  MODIFY `chat_memberid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
